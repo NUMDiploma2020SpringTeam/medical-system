cb9f34713e1e4accbdc867691d91622becd27892
Adiya <adiyaot@gmail.com>
2020-04-20 19:38:46 +0800

    Patient Controller change

--- src/main/java/microservices/diplom/medicalsystem/registration/controller/PatientController.java
+++ src/main/java/microservices/diplom/medicalsystem/registration/controller/PatientController.java
@@ -1,10 +1,19 @@
 package microservices.diplom.medicalsystem.registration.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import microservices.diplom.medicalsystem.registration.service.PatientService;
 
 @RestController
 @RequestMapping("/patients")
 final class PatientController {
 	
+	private final PatientService patientService;
+	
+	@Autowired
+	public PatientController(PatientService patientService) {
+		this.patientService = patientService;
+	}
 }
